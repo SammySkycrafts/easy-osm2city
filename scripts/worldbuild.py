32d3ec9d5fe18b8b5cd302f79b99880e70d106fa
merspieler <merspieler@airmail.cc>
2019-03-25 04:45:03 +0000

    Added progress -p param for worldbuild script
    Signed-off-by: merspieler <merspieler@airmail.cc>

--- scripts/worldbuild.py
+++ scripts/worldbuild.py
@@ -1,8 +1,72 @@
 #! /usr/bin/python
+# Copyright (C) 2018-2019 Merspieler, merspieler _at_ airmail.cc
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
 import os
 import sys
+import math
 
-pbf_path = sys.argv[1]
+argc = len(sys.argv)
+i = 1
+first = 1
+while i < argc:
+	if sys.argv[i] == "-p" or sys.argv[i] == "--progress":
+		try:
+			with open("projects/worldbuild/done") as f:
+				lines = f.readlines()
+				for line in lines:
+					match = re.findall("(-?[0-9]{1,3})_-?[0-9]{1,3}_-?[0-9]{1,3}_(-?[0-9]{1,3})", line
+					if match != []:
+
+						n = int(match[0][1])
+						w = int(match[0][0])
+
+						wm = w % 10
+						nm = n % 10
+
+						if nm = 0:
+							wm += 1
+
+						tile = wm * 10 + nm
+
+#						print("Worldbuild " + world + "% complete")
+						print("Current tile " + str(tile) + "% complete")
+						sys.exit(0)
+					else:
+						print("Unable to get progress")
+						sys.exit(1)
+		except IOError:
+			print("Unable to get progress")
+			sys.exit(1)
+	elif sys.argv[i] == "-h" or sys.argv[i] == "--help":
+		print("usage: build <project> [OPTIONS]")
+		print("Builds the tiles with osm2city")
+		print("")
+		print("OPTIONS")
+		print("  -h, --help      Shows this help and exit")
+		print("  -p, --progress  Shows progress and exit")
+		sys.exit(0)
+	else:
+		if first == 1:
+			first = 0
+			pbf_path = sys.argv[i]
+		else:
+			print("Unknown option " + sys.argv[i])
+			sys.exit(1)
+	i += 1
 
 # Expecting project
 os.system("mkdir -p projects/worldbuild/output/error")
